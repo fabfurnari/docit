402690d29b808942824fbd7134c39e4f405ddcf9
Fabrizio Furnari <fab.furnari@gmail.com>
2015-09-10 14:46:49 +0200

    added other fields

--- docit/api.py
+++ docit/api.py
@@ -1,5 +1,5 @@
 from docit import api
-from docit.model import db, DBSnippet
+from docit.model import db, Snippet
 from flask_restful import Resource, reqparse, abort, fields, marshal_with
 
 parser = reqparse.RequestParser()
@@ -12,19 +12,34 @@ parser.add_argument('tags',
 snippet_fields = {
     'id': fields.Integer,
     'data': fields.String,
-    'tags': fields.String
-    }
+    'tags': fields.String,
+    'user': fields.String,
+    'path': fields.String,
+    'hostname': fields.String,
+    'date_created': fields.DateTime,
+    'date_updated': fields.DateTime,
+}
 
 def abort_if_not_exists(snippet_id):
-    sn = db.session.query(DBSnippet).filter(DBSnippet.id == snippet_id).first()
+    sn = db.session.query(Snippet).filter(Snippet.id == snippet_id).first()
     if not sn:
         abort(404, message="Snippet %s does not exists" % snippet_id)
-        
+
+def create_snippet(snippet_id, args):
+    sn = Snippet(snippet_id,
+                 data=args['data'],
+                 tags=args['tags'],
+                 user='test user',
+                 path='test path',
+                 hostname='test hostname')
+    db.session.add(sn)
+    db.session.commit()
+    return sn
  
-class SnippetList(Resource):
+class SnippetListResource(Resource):
     @marshal_with(snippet_fields)
     def get(self):
-        l = db.session.query(DBSnippet).all()
+        l = db.session.query(Snippet).all()
         return l
 
     @marshal_with(snippet_fields)
@@ -33,25 +48,19 @@ class SnippetList(Resource):
         Create snippet
         '''
         args = parser.parse_args()
-        id_list = [x for x, in db.session.query(DBSnippet.id).all()]
+        id_list = [x for x, in db.session.query(Snippet.id).all()]
         if id_list:
             snippet_id = int(max(id_list) + 1)
         else:
             snippet_id = 0
-
-        sn = DBSnippet(snippet_id,
-                       data=args['data'],
-                       tags=args['tags'])
-        db.session.add(sn)
-        db.session.commit()
+        sn = create_snippet(snippet_id, args)
         return sn, 201
 
-class Snippet(Resource):
+class SnippetResource(Resource):
     @marshal_with(snippet_fields)
     def get(self, snippet_id):
         abort_if_not_exists(snippet_id)
-        s = db.session.query(DBSnippet).filter(DBSnippet.id == snippet_id).first()
-        
+        s = db.session.query(Snippet).filter(Snippet.id == snippet_id).first()
         return s
 
     @marshal_with(snippet_fields)
@@ -60,7 +69,7 @@ class Snippet(Resource):
         Update snippet
         '''
         args = parser.parse_args()
-        sn = db.session.query(DBSnippet).filter(DBSnippet.id == snippet_id).first()
+        sn = db.session.query(Snippet).filter(Snippet.id == snippet_id).first()
         sn.data = args['data']
         db.session.add(sn)
         db.session.commit()
@@ -72,11 +81,11 @@ class Snippet(Resource):
         Delete snippet
         '''
         abort_if_not_exists(snippet_id)
-        sn = db.session.query(DBSnippet).filter(DBSnippet.id == snippet_id).first()
+        sn = db.session.query(Snippet).filter(Snippet.id == snippet_id).first()
         db.session.delete(sn)
         db.session.commit()
         return {}, 204
 
 
-api.add_resource(SnippetList, '/api')
-api.add_resource(Snippet, '/api/<int:snippet_id>')
+api.add_resource(SnippetListResource, '/api')
+api.add_resource(SnippetResource, '/api/<int:snippet_id>')
--- docit/model.py
+++ docit/model.py
@@ -1,15 +1,27 @@
 from docit import db
+import datetime
 
-class DBSnippet(db.Model):
+class Snippet(db.Model):
     __tablename__ = 'Snippet'
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String())
     tags = db.Column(db.String())
+    user = db.Column(db.String())
+    path = db.Column(db.String())
+    hostname = db.Column(db.String())
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now())
+    date_updated = db.Column(db.DateTime, onupdate=datetime.datetime.now())
     
-    def __init__(self, snippet_id, data, tags):
+    def __init__(self,
+                 snippet_id, data, tags,
+                 user, path, hostname):
         self.id = snippet_id
         self.data = data
         self.tags = tags
+        self.user = user
+        self.path = path
+        self.hostname = hostname
+        
 
     def __repr__(self):
         return '<Snippet %r>' % self.id
