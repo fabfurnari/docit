6eff6a513bff15d109095d4dd9dfbb2f4e3a17d0
Fabrizio Furnari <fab.furnari@gmail.com>
2015-09-25 17:51:07 +0200

    start working on web interface

--- docit/webapp.py
+++ docit/webapp.py
@@ -1,7 +1,9 @@
 from docit import app
 from flask import render_template
+from docit.model import db, Snippet, Tag
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    sn = db.session.query(Snippet).all()
+    return render_template('index.html', snippets=sn)
 
