fd6d055dd55883b36cae884caa28d2cc0e76e200
Fabrizio Furnari <fab.furnari@gmail.com>
2015-10-09 22:59:33 +0200

    added delete functiokn

--- docit/static/js/main.js
+++ docit/static/js/main.js
@@ -1,11 +1,27 @@
-
 $(document).ready(function(){
+    $('.deleterow').unbind('click').bind('click', function(){
+        var $r_id = $(this).attr('id');
+        if (confirm("Removing snippet " + $r_id + "\nAre you sure?")) {
+            console.log("delete");
+            var $killrow = $(this).parent('tr');
+            $killrow.addClass("danger");
+            $killrow.fadeOut(2000, function(){
+                $.ajax({
+                    url: '/api/' + $r_id,
+                    type: 'DELETE',
+                    success: function(result) {
+                        console.log("success");
+                        $(this).remove();
+                    }
+                });
+            });
+        }
+    });
     $.fn.editable.defaults.mode = 'inline';
     $.fn.editable.defaults.ajaxOptions = {type: "PUT"};
     $('.editable-textbox').editable({
         type: 'textarea',
         pk: $(this.id),
-        url: '/api/' + $(this.id),
         title: 'Enter snippet text',
     });
     $('.editable-tag').editable({
