a6a6696cd8a223925a865bfd65cccd85a1ad8c02
Fabrizio Furnari <fab.furnari@gmail.com>
2017-06-03 22:43:43 +0200

    parse stdin, tags, ecc

--- client/go/client.go
+++ client/go/client.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 )
 
 type Configuration struct {
@@ -31,9 +33,34 @@ func LoadConfig(path string) Configuration {
 }
 
 func main() {
+	var (
+		text string
+	)
+
 	configurationFile := flag.String("config", "~/.docit.json", "Configuration file. Default: ~/.docit.json")
+	flagTag := flag.String("tags", "", "List of tags (comma separed)")
 	flag.Parse()
+
+	f, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatal("Stat() error", err)
+	}
+
+	if lText := flag.Args(); len(lText) != 0 {
+		text = strings.Join(lText[:], " ")
+	} else if (f.Size() > 0) || (f.Mode() & os.ModeNamedPipe != 0) {
+		// TODO: eventually strip newlines
+		bytes, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatal("Cannot read from STDIN")
+		}
+		text = string(bytes)
+	}  else {
+		log.Fatal("You must specify some text")
+	}
+
 	config := LoadConfig(*configurationFile)
-	fmt.Println(config.ServerUrl)
-	fmt.Println(config.ServerPort)
+	tags := strings.Split(*flagTag, ",")
+
+	fmt.Println(config.ServerUrl, tags, text)
 }
